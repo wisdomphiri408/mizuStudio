4d2eb7fb5bb2c7ac2c3e263cad5ba9deeb71811d
Wisdom Phiri <wisdomphiri408@gmail.com>
2025-09-18 13:36:22 +0200

    feat(ui-core): add responsive footer with social icons and layout fixes
    - Implemented footer component with brand info, quick links, and social icons
    - Integrated Lucide icons and TikTok SVG with styling support
    - Adjusted layout to ensure footer sticks to bottom of page
    - Fixed CSS variable typo and improved global text/link styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex flex-col`}>
         <Header />
-        <div className="pt-21">
+        <div className="flex-grow pt-21">
           {children}
         </div>
+        <Footer/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-[#3A86FF] mt-20 mb-4">Mizu Studio</h1>
+      <h1 className="text-emphasis mt-20 mb-4">Mizu Studio</h1>
 
       <p className="max-w-[500px] m-auto">
         Mizu Studio is a creative studio in Ndirande, Blantyre specializing in photography, videography, and photo editing. We capture weddings, birthdays, and special events with a professional touch.
