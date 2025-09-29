05cba2f79237b727d87c25e93e812677670b5775
Wisdom Phiri <wisdomphiri408@gmail.com>
2025-09-29 11:07:27 +0200

    Added the portfolio page to main

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from 'next';
+
+const PortfolioPage: NextPage = () => {
+    return (
+        <div className={'pt-4'}>
+            PortfolioPage
+        </div>
+
+    )
+}
+
+export default PortfolioPage;
